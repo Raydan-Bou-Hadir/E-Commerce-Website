ef833ac1049de1d039750b61b314b3db4e2c624a
Raydan Bou Hadir <raydanbouhadir00@gmail.com>
2024-10-18 17:24:07 +0300

    finish the media query for the offers component

--- src/components/Offers/Offers.jsx
+++ src/components/Offers/Offers.jsx
@@ -5,12 +5,12 @@ function Offers() {
   return (
     <main className='w-full h-auto grid py-6 px-6 md:px-10 lg:px-20 grid-cols-1 gap-8 lg:grid-cols-[50%_50%] lg:gap-[2%] bg-black'>
         <div className='relative'>
-            <img src={image1} alt="offers-image" className='w-full h-full object-cover overflow-hidden opacity-80' />
-            <div className='absolute top-[18%] left-[5%] flex flex-col gap-4'>
-                <h2 className='font-latin text-white text-5xl tracking-tight'>Still at weekend off light</h2>
-                <h1 className='text-white text-6xl font-semibold'>EXTRA<br/>75% OFF</h1>
-                <p className='text-white text-lg font-thin'> use premo code <span className='text-2xl font-semibold border-b-2'>Z9QAR21</span></p>
-                <button className='w-[60%] mt-1 border py-3 rounded-md text-white font-semibold hover:text-black hover:bg-white transition duration-300'>SHOP NOW</button>
+            <img src={image1} alt="offers-image" className='lg:w-full lg:h-full object-cover overflow-hidden opacity-80 md:h-[90%]' />
+            <div className='absolute top-[18%] left-[5%] flex flex-col gap-4 lg:gap-3 lg:top-[12%] md:top-[20%] md:gap-3 sm:gap-4 xsm:gap-0 xsm:top-[10%]'>
+                <h2 className='font-latin text-white text-5xl tracking-tight lg:text-4xl md:text-5xl sm:text-4xl xsm:text-xl'>Still at weekend off light</h2>
+                <h1 className='text-white text-6xl font-semibold lg:text-5xl md:text-6xl sm:text-5xl xsm:text-2xl'>EXTRA<br/>75% OFF</h1>
+                <p className='text-white text-lg font-thin md:text-xl sm:text-lg xsm:text-[10px]'> use premo code <span className='text-2xl font-semibold border-b-2 md:text-2xl sm:text-xl xsm:text-[14px]'>Z9QAR21</span></p>
+                <button className='md:w-[60%] md:py-3 md:mt-1 border rounded-md text-white font-semibold hover:text-black hover:bg-white transition duration-300 xsm:w-[80%] xsm:py-1 xsm:text-[14px] xsm:mt-2'>SHOP NOW</button>
             </div>
         </div>
         <div className='w-full'>
